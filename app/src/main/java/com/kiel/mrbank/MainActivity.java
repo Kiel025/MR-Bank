410e63ce629db51d2a9159ad9bf55bc0fdfb97e4
Kiel025 <kiel02505@gmail.com>
2023-11-27 22:08:53 -0300

    Saldo Activity

--- app/src/main/java/com/kiel/mrbank/MainActivity.java
+++ app/src/main/java/com/kiel/mrbank/MainActivity.java
@@ -2,13 +2,23 @@ package com.kiel.mrbank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.kiel.mrbank.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.imgSaldo.setOnClickListener( v -> {
+            startActivity(new Intent(this, SaldoActivity.class));
+        });
     }
 }
---
+++ app/src/main/java/com/kiel/mrbank/SaldoActivity.java
@@ -0,0 +1,19 @@
+package com.kiel.mrbank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kiel.mrbank.databinding.ActivitySaldoBinding;
+
+public class SaldoActivity extends AppCompatActivity {
+
+    private ActivitySaldoBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySaldoBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
